b3d43f5eac3ba8e7c92b614a2300c0de1cb9ab30
Deepak Singh <113229501+Deep2804@users.noreply.github.com>
2023-09-13 13:01:31 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"new strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3+3);
+console.log("Deepak");
+
+let name = "Deepak"
+let age = 20
+let isLoggedIn = false
+
+//Number =>2 to power 53
+//bigint 
+//string = ""
+//boolean => true or false
+//null => standalone value
+//undefined => 
+//symbol =>unique
+//object
+
+console.log(typeof undefined);
