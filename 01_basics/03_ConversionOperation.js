e41a229bb1106f5329396ac5eaf3a94546702f39
Deepak Singh <113229501+Deep2804@users.noreply.github.com>
2023-09-20 14:49:03 +0000

    Conversion of Data Types

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,8 +1,27 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" = 33
+//"33abc" = NaN
+// true = 1 ; false = 0
+
+let isLoggedIn = "deepak"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// true=>1 ; false=>0
+//"" = false
+//"deepak" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); 
+console.log(typeof stringNumber);
