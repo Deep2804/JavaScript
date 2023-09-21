c01e745e7e669f25327f8d6373eba3e034f5fa27
Deepak Singh <113229501+Deep2804@users.noreply.github.com>
2023-09-21 12:40:12 +0000

    Conversion Operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "deepak"
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // true=>1 ; false=>0
 //"" = false
@@ -23,5 +23,45 @@ console.log(booleanisLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber); 
-console.log(typeof stringNumber);
+// console.log(stringNumber); 
+// console.log(typeof stringNumber);
+
+//***********Operations **************
+
+let value = 3
+let negvalue = -value
+// console.log(value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Deepak"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+"2");
+// console.log("1"+2);
+// console.log("1"+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log((3+2)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+//link to study
+//https://tc39.es/ecma262/#sec-type-conversion
