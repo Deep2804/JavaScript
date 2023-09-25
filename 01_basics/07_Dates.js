894a993f46ca291ef5b8effaa8509314289579e7
Deepak Singh <113229501+Deep2804@users.noreply.github.com>
2023-09-25 16:55:56 +0000

    Date

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,26 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2000-04-09")
+ let myCreatedDate = new Date("01-14-2000")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
