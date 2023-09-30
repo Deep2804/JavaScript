4a91a5ec299f8ef371f2ae47a0462b214f8eae6b
Deepak Singh <113229501+Deep2804@users.noreply.github.com>
2023-09-30 17:53:08 +0000

    For In Loop

---
+++ 05_Iteration/04_Four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js : 'JavaScript',
+    cpp : 'C++',
+    rb : 'Ruby',
+    swift : 'Swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js","cpp","java","rb","py"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('CA',"Canada")
+// map.set('IN',"India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
