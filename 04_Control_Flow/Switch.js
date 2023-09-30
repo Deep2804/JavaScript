0b4829ca4d24f3e86d1b506bbd914ad1e2aac0f1
Deepak Singh <113229501+Deep2804@users.noreply.github.com>
2023-09-30 13:38:12 +0000

    Switch

---
+++ 04_Control_Flow/Switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    
+
+    default:
+        console.log("Default Case");
+        break;
+}
